41200fd2c89a055ef52877c7e878140228137bfe
keoni <kimkeonhwi991231@gmail.com>
2023-11-23 22:34:06 +0900

    keep

---
+++ src/main/java/MVC1/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,22 @@
+package MVC1.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-0
+ */
+@WebServlet(name = "RequestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
